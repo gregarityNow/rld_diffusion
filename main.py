cb4d282b5131c0cc0ae0b5be2fffdaefc9e1f1df
felix <felix.herron@gmail.com>
2023-02-17 16:12:31 +0100

    dump loss

--- main.py
+++ main.py
@@ -23,8 +23,10 @@ if opt.w > 0:
 else:
 	embedOptions = [None]
 
-for class_emb_dim in embedOptions[::(-1)**(opt.version%2)]:
-	for schedType in ["sigmoid","linear","quad"]:
-		diffModel, schedule = train_diff(cnn, test_loader, train_data=train_data, schedType=schedType,quickie=opt.quickie,
-							   version=opt.version,epochs= (50 if not opt.quickie else 3),class_emb_dim=class_emb_dim, w=opt.w)
+while True:
+	for class_emb_dim in embedOptions[::(-1)**(opt.version%2)]:
+		for schedType in ["sigmoid","linear","quad"]:
+			diffModel, schedule = train_diff(cnn, test_loader, train_data=train_data, schedType=schedType,quickie=opt.quickie,
+								   version=opt.version,epochs= (50 if not opt.quickie else 3),class_emb_dim=class_emb_dim, w=opt.w)
 
+	opt.version *= 10
