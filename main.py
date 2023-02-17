fb101a547f8ae9e2dec9626387ed8517a7201cbb
felix <felix.herron@gmail.com>
2023-02-17 03:26:18 +0100

    debug

--- main.py
+++ main.py
@@ -19,14 +19,14 @@ train_data = get_train_data(opt.quickie);
 res = []
 
 if opt.w > 0:
-	embedOptions = ["oneHot", 3, 10]
+	embedOptions = [3,"oneHot", 10]
 else:
 	embedOptions = [None]
 
 for class_emb_dim in embedOptions:
 	for schedType in ["sigmoid","linear","quad"]:
 		diffModel, schedule = train_diff(train_data=train_data, schedType=schedType,
-							   epochs= (30 if not opt.quickie else 3),class_emb_dim=class_emb_dim, w=opt.w)
+							   epochs= (50 if not opt.quickie else 3),class_emb_dim=class_emb_dim, w=opt.w)
 
 		fid_score, is_score = evaluate_diff_model(diffModel, cnn, test_loader, opt.w, schedule, numFakeIters=(50 if not opt.quickie else 2),batch_size=100)
 		d = {"w":opt.w,"class_emb_dim":class_emb_dim,"schedType":schedType,"fid":fid_score,"is_score":is_score}
